3fdc51b1ef034ba43c7f8cdd23c1b93d15785938
ThanhDang <thanhdang210203@gmail.com>
2023-07-16 16:40:27 +0700

    push

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,8 +7,6 @@ using Random = UnityEngine.Random;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] objectsToSpawn;
-    public Vector2 _BoundMin;
-    public Vector2 _BoundMax;
     public int spawnAmount;
     public float spawnRadius;
     public float spawnCollisionCheckRadius;
@@ -18,33 +16,42 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Start()
     {
+       
+    }
+
+    void SpawnEne()
+    {
         int i = 0;
-        int currentLimit = 0;
-        while(i < spawnAmount) 
+               int currentLimit = 0;
+               while(i < spawnAmount) 
+               {
+                   if (currentLimit >= maximumIteration)
+                   {
+                       Debug.Log("reached limit, cant spawn anymore");
+                       break;
+                   }
+                   int randomIndex = Random.Range(0, objectsToSpawn.Length);
+                   Vector2 SpawnPos = new Vector2(this.transform.position.x, this.transform.position.y) + Random.insideUnitCircle * spawnRadius;
+                   if (!Physics2D.OverlapCircle(SpawnPos,spawnCollisionCheckRadius))
+                   {
+                       Debug.Log("able to spawn");
+                       Instantiate(objectsToSpawn[randomIndex], SpawnPos, Quaternion.identity);
+                       i++;
+                   }
+                   else
+                   {
+                       Debug.Log("not able to spawn");
+                   }
+       
+                   currentLimit++;
+               }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
         {
-            if (currentLimit >= maximumIteration)
-            {
-                Debug.Log("reached limit, cant spawn anymore");
-                break;
-            }
-            int randomIndex = Random.Range(0, objectsToSpawn.Length);
-            Vector2 SpawnPos = new Vector2(Random.Range(_BoundMin.x, _BoundMax.x), Random.Range(_BoundMin.y, _BoundMax.y)) + Random.insideUnitCircle * spawnRadius;
-            if (!Physics2D.OverlapCircle(SpawnPos,spawnCollisionCheckRadius))
-            {
-                Debug.Log("able to spawn");
-                Instantiate(objectsToSpawn[randomIndex], SpawnPos, Quaternion.identity);
-                i++;
-            }
-            else
-            {
-                Debug.Log("not able to spawn");
-            }
-
-            currentLimit++;
+            SpawnEne();
         }
-
-        
-        
     }
 
     private void OnDrawGizmos()
