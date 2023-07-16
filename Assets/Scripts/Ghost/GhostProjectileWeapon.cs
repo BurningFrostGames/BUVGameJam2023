3014b8919a45ee308c340e10211e7b98245b6eb6
Khanh Duy <novelistoftheyear@gmail.com>
2023-07-16 12:00:08 +0700

    oops

--- Assets/Scripts/Ghost/GhostProjectileWeapon.cs
+++ Assets/Scripts/Ghost/GhostProjectileWeapon.cs
@@ -6,8 +6,8 @@ public class GhostProjectileWeapon : GhostWeapon
 {
     public void SpawnProjectile()
     {
-        var bullet = Instantiate(Projectile, spawnTransform.position, spawnTransform.rotation);
-        bullet.AddForce(spawnTransform.up * speed);
-        Destroy(bullet, 10f);
+        // var bullet = Instantiate(Projectile, spawnTransform.position, spawnTransform.rotation);
+        // bullet.AddForce(spawnTransform.up * speed);
+        // Destroy(bullet, 10f);
     }
 }
